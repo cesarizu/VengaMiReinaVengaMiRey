b1285dd23465fdaad61988bc29147f2c2febed7a
Cesar Izurieta <cesar@caih.org>
2024-01-27 11:48:17 -0500

    Comida

--- scenes/titleScreen/titleScreen.gd
+++ scenes/titleScreen/titleScreen.gd
@@ -1,10 +1,5 @@
 extends CanvasLayer
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
+func _input(event: InputEvent) -> void:
+	if event.is_pressed():
 		get_tree().change_scene_to_file("res://scenes/mainMenu/mainMenu.tscn")
