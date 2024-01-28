228d70449a4beda201833c8f90e1d28678b374e4
darmusica <98622542+darmusica@users.noreply.github.com>
2024-01-28 16:39:54 -0500

    MusicaJuego

--- autoloads/audio_manager.gd
+++ autoloads/audio_manager.gd
@@ -5,7 +5,11 @@ extends Node
 func _ready():
 	musica_menu.play()
 	GameManager.game_started.connect(_on_game_started)
+	GameManager.game_ended.connect(_on_game_ended)
 	
 func _on_game_started():
 	musica_menu.stop()
 	musica_juego.play()
+func _on_game_ended():
+	musica_menu.play()
+	musica_juego.stop()
